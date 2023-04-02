f1ec9c7464048e499c6e60c362309f8d8b7985b4
hwani <ghksl12q@gmail.com>
2023-04-02 22:04:52 +0900

    front-end_javascript-api

--- main.js
+++ main.js
@@ -1,18 +1,43 @@
 
-let news = []
-// [1] api불러오기
-const getLatestNews =async()=> {
-  let url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business`);
-  
+const getNews =async()=> {
   let header = new Headers({'x-api-key':'JJuWDXzbFQMEqpjpqDKtNwDUzaZgNpmQP4mr400GiUg'});
-
   let response = await fetch(url,{headers:header});
   let data = await response.json()
   news = data.articles;
-  console.log(news);
 
   render();
+}
+
+let url;
+let menus = document.querySelectorAll(".menus button");
+let searchButton = document.getElementById("search-button");
+
+// 이벤트 함수 만들기(서치)
+menus.forEach(menu => menu.addEventListener("click", (event)=> getNewsByTopic(event)))
+
+let news = []
+// [1] api불러오기
+const getLatestNews =()=> {
+  url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&topic=business`);
+  getNews();
 };
+
+// 서치 이벤트
+const getNewsByKeyword =()=> {
+  let keyword = document.getElementById("search-input").value
+  url = new URL(`https://api.newscatcherapi.com/v2/search?q=${keyword}&countries=KR&page_size=10`);
+  getNews();
+}
+
+// 서치 함수
+searchButton.addEventListener("click", getNewsByKeyword);
+
+// 서치 이벤트
+const getNewsByTopic =(event)=> {
+  let topic = event.target.textContent.toLowerCase()
+  url = new URL(`https://api.newscatcherapi.com/v2/latest_headlines?countries=KR&page_size=10&topic=${topic}`);
+  getNews();
+}
 
 // [2] 뉴스 가져와서 화면에 뿌리기
 const render =()=> {
@@ -34,4 +59,7 @@ const render =()=> {
 
   document.querySelector("#news-board").innerHTML = newsHTML;
 }
+
+
+
 getLatestNews();
